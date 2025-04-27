a49560ddf31b042334d38d767419eb27dca597b0
Aryan <aryanbhatiya.github@gmail.com>
2025-04-27 23:40:50 +0530

    Started Responsive Design

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,6 +5,8 @@ function Navbar() {
     return(
         <nav className={styles.navigation}>
           <div className={`${styles.logo}`}><img src="images/portfolio_logo.PNG"></img></div>
+
+          {/* in case of desktop */}
           <div className={styles.navLinks}>
             <Link to="/" className={`${styles.btn} h-6`}>Home</Link>
             <Link to="/about" className={`${styles.btn} h-6`}>About</Link>
@@ -14,6 +16,9 @@ function Navbar() {
           <div className={styles.downbtn}>
             <a href="Aryan-Resume.docx" download className={`${styles.cv} h-6`}><img src="images/download_icon.png"></img>Download CV</a>
           </div>
+
+          {/* in case of mobile */}
+          <div className={styles.menuIcon}><img src="images/hamburger-menu.png"></img></div>
         </nav>
     )
 }
